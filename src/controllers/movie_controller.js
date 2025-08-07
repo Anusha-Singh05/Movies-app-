284bd368fc17b83b3988c2f10611e85783f8c22c
Anusha-Singh05 <anushasingh7820@gmail.com>
2025-08-07 19:54:15 +0530

    Add movies

--- src/controllers/movie_controller.js
+++ src/controllers/movie_controller.js
@@ -2,8 +2,20 @@ import Movie from "../models/movie.js";
 
 export async function getMovies(req, res) {
     try {
-        const n = req.query.n || 10;
-        const movies = await Movie.find({}).limit(n)
+        const n = req.query.n || 2;
+        const q = req.query.q || '';
+        const page = req.query.page || 1;
+        const movies = await Movie.find({
+            $and: [
+                {
+                    title: { $regex: q, $options: "i" },
+                },
+                {
+                    plot: { $regex: q, $options: "i" },
+                }
+            ],
+        }).limit(n)
+          .skip(n * (page - 1));
         res.json({
             "movies": movies
         })

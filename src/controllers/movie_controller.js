1fdeee25d7e523f1b5767e61a756e9137e09b64f
Anusha-Singh05 <anushasingh7820@gmail.com>
2025-08-13 22:00:54 +0530

    Add features

--- src/controllers/movie_controller.js
+++ src/controllers/movie_controller.js
@@ -15,9 +15,12 @@ export async function getMovies(req, res) {
                 }
             ],
         }).limit(n)
-          .skip(n * (page - 1));
+            .skip(n * (page - 1));
+
+        const totalMovies = await Movie.countDocument();
         res.json({
-            "movies": movies
+            total: totalMovies,
+            movies: movies,
         })
     } catch (error) {
         res.json({
@@ -25,3 +28,44 @@ export async function getMovies(req, res) {
         })
     }
 }
+
+export async function getMovieById(req, res) {
+    const id = req.params.id;
+    const movie = await Movie.findById(id);
+    res.json({
+        movie: movie,
+    });
+}
+
+export async function createMovie(req, res) {
+    const { title, poster } = req.body;
+    const movie = await Movie.create({ title, poster });
+    res.json({
+        movie: movie,
+        message: "Movie craeted successfully",
+    });
+}
+
+export async function deleteMovie(req, res) {
+    const id = req.params.id;
+    const movie = await Movie.findbyIdAndDelete(id);
+    res.json({
+        movie: movie,
+        message: "Movie deleted successfully",
+    });
+}
+
+export async function updateMovie(req, res) {
+    const id = req.params.id;
+    const { title, poster } = req.body;
+    const movie = await Movie.findByIdAndUpdate(
+        id,
+        { title, poster },
+        { new: true }
+    );
+    res.json({
+        movie: movie,
+        message: "Movie updated successfully",
+    });
+}
+
--- src/routers/movie_router.js
+++ src/routers/movie_router.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { getMovies } from '../controllers/movie_controller.js'
-getMovies
+import { createMovie, deleteMovie, getMovieById, updateMovie } from '../controllers/movie_controller.js';
+getMovies;
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", getMovies)
+router.get("/", getMovies);
+router.get("/:id", getMovieById);
+router.get("/", createMovie);
+router.get("/:id", deleteMovie);
+router.get("/:id", updateMovie);
 
-export default router
+export default router;
